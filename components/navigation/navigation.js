cc1e50132a19187f4fe81d11ed11f914aea7c286
sabbir ahmmed <ahsabbir103@gmail.com>
2021-07-26 01:11:14 +0600

    done

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -5,7 +5,7 @@ export default function navigation() {
     //     console.log("navigation component")
     // })
     return (
-        <div className="flex w-40 h-full justify-center items-center">
+        <div className="flex w-40 h-full justify-center items-center bg-green-900">
                 <ul className="flex-col">
                     <li className="px-2 py-1 mx-1 font-Qahiri font-bold text-red-500 tracking-wider uppercase hover:bg-blue-700 rounded-md">
                         <a href="#">Home</a>
