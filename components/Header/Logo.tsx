a555a46979c95b0dd89fb76e695609a53f93c071
Ahmmed Sabbir <ahsabbir103@gmail.com>
2021-09-03 03:05:28 +0600

    updated Image

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,11 +1,12 @@
-
+import logo from './images/logo.png';
+import Image from 'next/image';
 const Logo = () => {
 
     return (
         
                         <div className="w-1/2 pl-2 md:pl-0">
                             <a className="text-gray-100 text-base xl:text-xl no-underline hover:no-underline font-bold" href="#">
-                            <img src={"./images/logo.png"} height="20" width="20" alt="Picture of the author" /> Task Time
+                            <Image src={logo} height="20" width="20" alt="Picture of the author" /> Task Time
                             </a>
                         </div>
                        
