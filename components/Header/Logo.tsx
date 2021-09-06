27d28641fe101eee4707fabb1eb8e656b75d6310
Ahmmed Sabbir <ahsabbir103@gmail.com>
2021-09-07 05:35:07 +0600

    init again

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,12 +1,11 @@
-import logo from '../../public/images/logo.png';
-import Image from 'next/image';
+
 const Logo = () => {
 
     return (
         
                         <div className="w-1/2 pl-2 md:pl-0">
                             <a className="text-gray-100 text-base xl:text-xl no-underline hover:no-underline font-bold" href="#">
-                            <Image src={logo} height="20" width="20" alt="Picture of the author" /> Task Time
+                                <i className="fas fa-moon text-blue-400 pr-3"></i> Admin Dark Mode
                             </a>
                         </div>
                        
--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} **/
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['i.pravatar.cc'],
-    loader: 'imgix',
-    path: 'https://i.pravatar.cc',
-  },
 }
-
